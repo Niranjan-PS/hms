6c1b055916b105d8bbf3b830f748795cf44551c8
niranjanps <niranjanpsniranjan0@gmail.com>
2025-05-28 09:21:59 +0530

    updated appjs

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import errorHandler from './middlewares/error.js'
 const app = express();
 
 
-app.use(cors({ origin: 'http://localhost:4200' }))
+app.use(cors({ origin: 'hms-frontend-iota.vercel.app' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
