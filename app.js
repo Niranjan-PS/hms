914f79ac847401b316bf5c6e1dc156c0a0100b49
niranjanps <niranjanpsniranjan0@gmail.com>
2025-05-28 09:28:48 +0530

    updated cors

--- app.js
+++ app.js
@@ -9,8 +9,24 @@ import errorHandler from './middlewares/error.js'
 
 const app = express();
 
+const allowedOrigins = [
+  'https://hms-frontend-iota.vercel.app', 
+  'http://localhost:4200' 
+];
 
-app.use(cors({ origin: 'hms-frontend-iota.vercel.app' }))
+app.use(cors({
+  origin: (origin, callback) => {
+    
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
